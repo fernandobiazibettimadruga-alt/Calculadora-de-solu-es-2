23b622c970479032811c18c65bdb5343eeb6382c
Fernando Biazibetti <fernandobiazibettimadruga@gmail.com>
2026-04-02 14:10:27 +0000

    message commit

--- app.py/app.py
+++ app.py/app.py
@@ -45,5 +45,9 @@ def calculadora():
 
     return render_template("index.html", resultado=resultado)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    print(f"🚀 Subindo Flask na porta {port}")
+    app.run(host="0.0.0.0", port=port, debug=True)
